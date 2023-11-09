49547ebc62258db3930c9538dce6233293860202
Lynn Wolters <lynn.wolters27@gmail.com>
2023-11-09 15:21:57 +0100

    responsive

--- my-app/src/routes/components/Header.svelte
+++ my-app/src/routes/components/Header.svelte
@@ -1,76 +1,59 @@
 <header>
-    <div>
-        <h1>The dangers <br> of the world <br> wide web</h1>
-    </div>
-    <div>
-        <p>By Lynn Wolters<p>
+    <nav class="p-text-small">
+        <p>By Lynn Wolters</p>
         <p>October, 2023</p>
-    </div>
-    <div>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br> <br>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi. 
-        </p>
-    </div>
+    </nav>
+    <h1 class="title-normal">The dangers <br> of the world <br> wide web</h1>
+    <p class="p-text-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br> <br> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
 </header>
 
 <style>
+    /* HEADER */
     header {
-        display: flex;
-        flex-direction: column;
-        margin: 0 2em 0 2em;
-    }
-
-    header div:nth-of-type(1) {
-        order: 2;
         display: grid;
         grid-template-columns: repeat(12, 1fr);
-        gap: 2em;
-        margin-top: 8%;
+        column-gap: 2em;
+        margin: 2em 2em 12em 2em;
     }
 
-    header div:nth-of-type(1) h1 {
+    /* NAV */
+    nav {
+        grid-column-start: 1;
+        grid-column-end: 13;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    /* TITLE */
+    h1 {
         grid-column-start: 2;
         grid-column-end: 12;
-        color: var(--color-2);
-        font-family: 'Perfect DOS VGA 437';
-        font-size: 9em;
-        letter-spacing: -.05em;
-        text-transform: uppercase;
-        line-height: 80%;
+        margin: .6em 0;
         text-align: center;
     }
 
-    header div:nth-of-type(2) {
-        order: 1;
-        display: flex;
-        justify-content: space-between;
-        margin-top: 2em;
-    }
-
-    header div:nth-of-type(2) p {
-        color: var(--color-3);
-        font-family: 'Coder\'s Crux';
-        font-size: 1.25em;
-        text-transform: uppercase;
-        line-height: 70%;
-    }
-
-    header div:nth-of-type(3) {
-        order: 3;
-        display: grid;
-        grid-template-columns: repeat(12, 1fr);
-        gap: 2em;
-        margin-top: 4%;
-    }
-
-    header div:nth-of-type(3) p {
+    /* TEXT */
+    p {
         grid-column-start: 4;
         grid-column-end: 10;
-        color: var(--color-3);
-        font-family: 'Coder\'s Crux';
-        font-size: 1.8em;
-        text-transform: uppercase;
-        line-height: 70%;
+    }
+
+    @media (max-width: 800px) {
+        h1 {
+            grid-column-start: 1;
+            grid-column-end: 13;
+        }
+
+        p {
+            grid-column-start: 3;
+            grid-column-end: 11;
+        }
+    }
+
+    @media (max-width: 560px) {
+        p {
+            grid-column-start: 1;
+            grid-column-end: 13;
+        }
     }
 </style>
---
+++ my-app/src/routes/components/Oefening.svelte
@@ -0,0 +1,38 @@
+<!-- <script>
+    import { onMount } from "svelte"
+    import * as d3 from "d3"
+
+    const data = [4, 12, 44, 32, 54, 65, 88]
+
+    onMount(async () => {
+
+        const div = d3.create("div")
+            .style("font", "10px sans-serif")
+            .style("text-align", "right")
+            .style("color", "white")
+
+        div.selectAll("div")
+            .data(data)
+            .join("div")
+            .style("background", "steelblue")
+            .style("padding", "4px")
+            .style("margin", "2px")
+            .style("width", d => `${d * 10}px`)
+            .text(d => d)
+        
+        document.querySelector(".chart-container").appendChild(div.node())
+
+    })
+</script>
+
+<div class="chart-container"></div>
+
+<style>
+    .chart-container {
+        height: 100vh;
+        background-color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style> -->
